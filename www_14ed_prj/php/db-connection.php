04818ec0ca1b9e1250a194496a217e40c93ade5f
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-06-08 10:06:33 +0300

    use config file for db connection

--- www_14ed_prj/php/db-connection.php
+++ www_14ed_prj/php/db-connection.php
@@ -1,11 +1,15 @@
 <?php
-function openConnection()
-{
-  $dbhost = "localhost";
-  $dbuser = "ivayloWEB";
-  $dbpass = "********";
-  $dbname = "ascii_sciinema";
-  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+function openConnection() {
+  $configs = include('../php/config.php');
+
+  $host = $configs->DB_HOST;
+  $user = $configs->DB_USER;
+  $password = $configs->DB_PASSWORD;
+  $name = $configs->DB_NAME;
+  $conn = new PDO("mysql:host=$host;dbname=$name", $user, $password);
+
   return $conn;
 }
+
 ?>
