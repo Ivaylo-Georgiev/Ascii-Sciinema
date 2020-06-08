72bb5c925e2aee29d8d61fbbc8b2413f830a5715
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-06-08 10:05:49 +0300

    create config.php

---
+++ www_14ed_prj/php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return (object) array(
+  'SITE_FN' => 62198,
+  'SITE_CREATOR' => 'Ivaylo Georgiev',
+  'SITE_ADMIN_EMAIL' => 'ivajlodg1@uni-sofia.bg',
+  'SITE_INFO' => 'This is a project for the Web Technologies course at FMI, lead by professor Milen Petrov',
+
+  'ROOT_FOLDER' => 'C:\Program Files\xampp\htdocs\Ascii_Sciinema\www_14ed_prj',
+  'DB_USER' => '62198_user',
+  'DB_PASSWORD' => '62198_password',
+  'DB_NAME' => 'www_14ed_62198_ascii_sciinema',
+  'DB_HOST' => 'localhost',
+);
+
+?>
