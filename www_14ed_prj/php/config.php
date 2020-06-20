d7c7a2b96cdbd5bbb462b44191bf17c34b6e0155
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-06-20 20:46:48 +0300

    add resources absolute path in config.php

--- www_14ed_prj/php/config.php
+++ www_14ed_prj/php/config.php
@@ -6,7 +6,7 @@ return (object) array(
   'SITE_ADMIN_EMAIL' => 'ivajlodg1@uni-sofia.bg',
   'SITE_INFO' => 'This is a project for the Web Technologies course at FMI, lead by professor Milen Petrov',
 
-  'ROOT_FOLDER' => 'C:\Program Files\xampp\htdocs\Ascii_Sciinema\www_14ed_prj',
+  'DB_RESOURCES' => 'C:\Program Files\xampp\htdocs\Ascii_Sciinema\www_14ed_prj\resources\db',
   'DB_USER' => '62198_user',
   'DB_PASSWORD' => '62198_password',
   'DB_NAME' => 'www_14ed_62198_ascii_sciinema',
